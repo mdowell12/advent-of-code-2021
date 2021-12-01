d5bf55b6141b2acc86e8ce5e005d8d33bda83745
Matt Dowell <mattdowell12@gmail.com>
2021-12-01 06:48:49 -0500

    Day 1

--- solutions/1/run.py
+++ solutions/1/run.py
@@ -2,25 +2,51 @@ from solutions.get_inputs import read_inputs
 
 
 def run_1(inputs):
-    pass
+    nums = [int(i) for i in inputs]
+    increases = 0
+    previous = None
+    for i in nums:
+        if previous is not None and i > previous:
+            increases += 1
+        previous = i
+
+    return increases
 
 
 def run_2(inputs):
-    pass
+    nums = [int(i) for i in inputs]
+    increases = 0
+    previous = (nums[0], nums[1], nums[2])
+    for i in nums[3:]:
+        next = (previous[1], previous[2], i)
+        if sum(next) > sum(previous):
+            increases += 1
+        previous = next
+
+    return increases
 
 
 def run_tests():
     test_inputs = """
-
+    199
+    200
+    208
+    210
+    200
+    207
+    240
+    269
+    260
+    263
     """.strip().split('\n')
 
     result_1 = run_1(test_inputs)
-    if result_1 != 0:
+    if result_1 != 7:
         raise Exception(f"Test 1 did not past, got {result_1}")
 
-    # result_2 = run_2(test_inputs)
-    # if result_2 != 0:
-    #     raise Exception(f"Test 2 did not past, got {result_2}")
+    result_2 = run_2(test_inputs)
+    if result_2 != 5:
+        raise Exception(f"Test 2 did not past, got {result_2}")
 
 
 if __name__ == "__main__":
@@ -28,8 +54,8 @@ if __name__ == "__main__":
 
     input = read_inputs(1)
 
-    # result_1 = run_1(input)
-    # print(f"Finished 1 with result {result_1}")
+    result_1 = run_1(input)
+    print(f"Finished 1 with result {result_1}")
 
-    # result_2 = run_2(input)
-    # print(f"Finished 2 with result {result_2}")
+    result_2 = run_2(input)
+    print(f"Finished 2 with result {result_2}")
