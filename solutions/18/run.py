1d63e5b5816db0f222079d130bc74704b56f4635
Matt Dowell <mattdowell12@gmail.com>
2021-12-18 17:43:23 -0800

    Day 18

--- solutions/18/run.py
+++ solutions/18/run.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+from itertools import permutations, combinations
 import json
 from math import ceil
 from math import floor
@@ -7,14 +9,28 @@ from solutions.get_inputs import read_inputs
 
 def run_1(inputs):
     numbers = [_parse_line(i) for i in inputs]
-    import pdb; pdb.set_trace()
     summed = _get_sum(numbers)
-
     return _magnitude(summed)
 
 
 def run_2(inputs):
-    pass
+    numbers = [_parse_line(i) for i in inputs]
+    maximum = None
+    for pair in combinations(numbers, 2):
+        summed = _get_sum(deepcopy(pair))
+        mag = _magnitude(summed)
+        if maximum is None or mag > maximum:
+            maximum = mag
+
+        # Not commutative so try the other direction too
+        reverse_pair = deepcopy([pair[1], pair[0]])
+        summed = _get_sum(reverse_pair)
+        mag = _magnitude(summed)
+        if maximum is None or mag > maximum:
+            maximum = mag
+
+    return maximum
+
 
 
 def _parse_line(line):
@@ -22,11 +38,11 @@ def _parse_line(line):
 
 
 def _get_sum(numbers):
-    import pdb; pdb.set_trace()
     result = numbers[0]
+    result = _reduce(result)
     for number in numbers[1:]:
         added = _add(result, number)
-        result, _, _ = _reduce(added)
+        result = _reduce(added)
     return result
 
 
@@ -35,14 +51,20 @@ def _add(pair_left, pair_right):
 
 
 def _reduce(pairs, depth=0):
-    import pdb; pdb.set_trace()
+    # print("reducing ", pairs)
     result = pairs
     while True:
         before = result
-        result, _, _ = _explode(result, depth=depth)
+        result, a, b, did_explode = _explode(result, depth=depth)
+        # print(result)
+        if did_explode:
+            continue
         result, changed = _split(result)
+        # print(result)
+        # print()
         if before == result:
             break
+    # print("finished ", result, " \n\n\n")
     return result
 
 
@@ -54,100 +76,76 @@ def _split(pairs):
             return _split_num(pairs), True
     else:
         left, changed_left = _split(pairs[0])
-        right, changed_right = _split(pairs[1])
+        if not changed_left:
+            right, changed_right = _split(pairs[1])
+        else:
+            right, changed_right = pairs[1], False
         return [left, right], (changed_left or changed_right)
 
 
-def _explode(pairs, depth=0):
-    # print('   '*depth, pairs)
+'''
+always a right remaining and a left remaining after an exploded pair
+we care which side of an outer pair an exploded pair came from
+after the explosion, we can always immediately increment the *other* side of the pair
+going further outwards, we should continue trying to increment something on the exploded side
+
+'''
+def _try_adding_right(right, amount):
+    if isinstance(right, int):
+        return right + amount, None
+    new_right, amount = _try_adding_right(right[0], amount)
+    right[0] = new_right
+    return right, amount
+
+
+def _try_adding_left(left, amount):
+    if isinstance(left, int):
+        return left + amount, None
+    new_left, amount = _try_adding_left(left[1], amount)
+    left[1] = new_left
+    return left, amount
+
+
+def _explode(pairs, depth=0, did_explode=False):
+    # print(depth, '     '*depth, pairs)
     if isinstance(pairs, int):
-        return pairs, None, None, None
-        # if pairs < 10:
-        #     return pairs, None, None
-        # else:
-        #     return _split(pairs), None, None
+        return pairs, None, None, did_explode
     elif depth == 4:
-        import pdb; pdb.set_trace()
-        return 0, pairs[0], pairs[1], 32
-    else:
-        left, add_left_1, add_right_1, from_i_1 = _explode(pairs[0], depth=depth+1)
-        right, add_left_2, add_right_2, from_i_2 = _explode(pairs[1], depth=depth+1)
-        # left, add_left_1, add_right_1 = _reduce(pairs[0], depth=depth+1)
-        # right, add_left_2, add_right_2 = _reduce(pairs[1], depth=depth+1)
-
-        if depth == 0:
+        if not (isinstance(pairs[0], int) and isinstance(pairs[1], int)):
             import pdb; pdb.set_trace()
-        result = [left, right]
-        result, add_left_remaining, add_right_remaining = _distribute_remaining(result, add_left_1, add_right_1, from_i_1)
-        result, add_left_remaining, add_right_remaining = _distribute_remaining(result, add_left_2, add_right_2, from_i_2)
+            raise Exception("Too deep baby ", pairs)
+        # print("exploding ", pairs)
+        return 0, pairs[0], pairs[1], True
+    else:
+        left, add_left_1, add_right_1, did_explode = _explode(pairs[0], depth=depth+1, did_explode=did_explode)
+        if not did_explode:
+            right, add_left_2, add_right_2, did_explode = _explode(pairs[1], depth=depth+1, did_explode=did_explode)
+        else:
+            right, add_left_2, add_right_2 = pairs[1], None, None
 
+        add_left_remaining = None
+        add_right_remaining = None
 
-        # add_left_remaining = None
-        # if add_left_1:
-        #     print('   l1'*depth, add_left_1, pairs[0], left)
-        #     if isinstance(pairs[0], int):
-        #         left += add_left_1
-        #     else:
-        #         add_left_remaining = add_left_1
-        # elif add_left_2:
-        #     print('   l2'*depth, add_left_2, pairs[0], left)
-        #     if isinstance(pairs[0], int):
-        #         left += add_left_2
-        #     else:
-        #         add_left_remaining = add_left_2
-        #
-        # add_right_remaining = None
-        # if add_right_1:
-        #     print('   r1'*depth, add_right_1, pairs[1], right)
-        #     # import pdb; pdb.set_trace()
-        #     if isinstance(pairs[1], int):
-        #         right += add_right_1
-        #     else:
-        #         add_right_remaining = add_right_1
-        # elif add_right_2:
-        #     # import pdb; pdb.set_trace()
-        #     print('   r2'*depth, add_right_2, pairs[1], right)
-        #     if isinstance(pairs[1], int):
-        #         right += add_right_2
-        #     else:
-        #         add_right_remaining = add_right_2
-        # result = [left, right]
-        from_i = None
-        if add_left_1 or add_right_1:
-            from_i = from_i_1
-        elif add_left_2 or add_right_2:
-            from_i = from_i_2
+        if add_right_1 is not None:
+            # came_from_left_and_needs_to_go_right
+            right, add_right_remaining = _try_adding_right(right, add_right_1)
+        if add_left_1 is not None:
+            # came_from_left_and_needs_to_go_left
+            # Since came from left, do not modify left, wait until higher level
+            add_left_remaining = add_left_1
+        if add_right_2 is not None:
+            # came_from_right_and_needs_to_go_right
+            # Since came from right, do not modify right, wait until higher level
+            add_right_remaining = add_right_2
+        if add_left_2 is not None:
+            # came_from_right_and_needs_to_go_left
+            left, add_left_remaining = _try_adding_left(left, add_left_2)
 
-        print('   '*depth, result, add_left_remaining, add_right_remaining, from_i)
-        return result, add_left_remaining, add_right_remaining, from_i
-
-def _distribute_remaining(pairs, add_left, add_right, from_i):
-    pair_string = json.dumps(pairs)
-    if add_left:
-        i = from_i
-        while i < len(pair_string):
-            if pair_string[i].isdigit():
-                val = int(pair_string[i]) + add_left
-                pair_string = pair_string[:i] + str(val) + pair_string[i+1:]
-                add_left = None
-                break
-            else:
-                i+=1
-
-    if add_right:
-        i = from_i
-        while i >= 0:
-            if pair_string[i].isdigit():
-                val = int(pair_string[i]) + add_right
-                pair_string = pair_string[:i] + str(val) + pair_string[i+1:]
-                add_right = None
-                break
-            else:
-                i+=1
-    return json.loads(pair_string), add_left, add_right
+        return [left, right], add_left_remaining, add_right_remaining, did_explode
 
 
 def _split_num(number):
+    # print("splitting ", number)
     left = floor(number / 2)
     right = ceil(number / 2)
     return [left, right]
@@ -168,6 +166,15 @@ def _magnitude(pairs):
 
 
 def run_tests():
+
+    if (result := _try_adding_right([1,1], 7)) != ([8,1], None):
+        raise Exception(result)
+
+    if (result := _try_adding_right([[1,1], 1], 7)) != ([[8,1],1], None):
+        raise Exception(result)
+
+    if (result := _try_adding_left([[1,1], 1], 7)) != ([[1,1],8], None):
+        raise Exception(result)
 
     if (result := _add([1,2],[[3,4],5])) != [[1,2],[[3,4],5]]:
         raise Exception(result)
@@ -190,11 +197,11 @@ def run_tests():
     if (result := _magnitude([[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]])) != 3488:
         raise Exception(result)
 
-    # if (result := _explode([1,4])) != ([1,4], None, None):
-    #     raise Exception(result)
+    if (result := _explode([1,4])) != ([1,4], None, None, False):
+        raise Exception(result)
     #
-    # if (result := _explode([[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]])[0]) != [[[[0,7],4],[[7,8],[6,0]]],[8,1]]:
-    #     raise Exception(result)
+    if (result := _explode([[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]])[0]) != [[[[0,7],4],[[7,8],[6,0]]],[8,1]]:
+        raise Exception(result)
 
     if (result := _split([4,10])) != ([4,[5,5]], True):
         raise Exception(result)
@@ -202,14 +209,20 @@ def run_tests():
     if (result := _split([11,4])) != ([[5,6],4], True):
         raise Exception(result)
 
-    # if (result := _reduce([[[[[9,8],1],2],3],4])) != [[[[0,9],2],3],4]:
-    #     raise Exception(result)
-    #
-    # if (result := _reduce([7,[6,[5,[4,[3,2]]]]])) != [7,[6,[5,[7,0]]]]:
-    #     raise Exception(result)
-    #
-    # if (result := _reduce([[6,[5,[4,[3,2]]]],1])) != [[6,[5,[7,0]]],3]:
-    #     raise Exception(result)
+    if (result := _split([11,11])) != ([[5,6],11], True):
+        raise Exception(result)
+
+    if (result := _reduce([[[[[9,8],1],2],3],4])) != [[[[0,9],2],3],4]:
+        raise Exception(result)
+
+    if (result := _reduce([7,[6,[5,[4,[3,2]]]]])) != [7,[6,[5,[7,0]]]]:
+        raise Exception(result)
+
+    if (result := _reduce([[6,[5,[4,[3,2]]]],1])) != [[6,[5,[7,0]]],3]:
+        raise Exception(result)
+
+    if (result := _reduce([[6,[5,[4,[3,2]]]],1])) != [[6,[5,[7,0]]],3]:
+        raise Exception(result)
 
     if (result := _reduce([[[[[4,3],4],4],[7,[[8,4],9]]], [1,1]])) != [[[[0,7],4],[[7,8],[6,0]]],[8,1]]:
         raise Exception(result)
@@ -217,11 +230,49 @@ def run_tests():
     if (result := _get_sum([[1,1],[2,2],[3,3],[4,4]])) != [[[[1,1],[2,2]],[3,3]],[4,4]]:
         raise Exception(result)
 
-    # if (result := _reduce([[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]])[0]) != [[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]:
-    #     raise Exception(result)
+    test = [json.loads(i) for i in """
+    [1,1]
+    [2,2]
+    [3,3]
+    [4,4]
+    """.strip().split('\n')]
+    if (result := _get_sum(test)) != [[[[1,1],[2,2]],[3,3]],[4,4]]:
+        raise Exception(result)
 
-    # if (result := _reduce([[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]])[0]) != [[3,[2,[8,0]]],[9,[5,[7,0]]]]:
-    #     raise Exception(result)
+    test = [json.loads(i) for i in """
+    [1,1]
+    [2,2]
+    [3,3]
+    [4,4]
+    [5,5]
+    """.strip().split('\n')]
+    if (result := _get_sum(test)) != [[[[3,0],[5,3]],[4,4]],[5,5]]:
+        raise Exception(result)
+
+    test = [json.loads(i) for i in """
+    [1,1]
+    [2,2]
+    [3,3]
+    [4,4]
+    [5,5]
+    [6,6]
+    """.strip().split('\n')]
+    if (result := _get_sum(test)) != [[[[5,0],[7,4]],[5,5]],[6,6]]:
+        raise Exception(result)
+
+    test = [json.loads(i) for i in """
+    [[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]
+    [[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
+    """.strip().split('\n')]
+    if (result := _get_sum(test)) != [[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]]:
+        raise Exception(result)
+
+    test = [json.loads(i) for i in """
+    [[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
+    [7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
+    """.strip().split('\n')]
+    if (result := _get_sum(test)) != [[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]:
+        raise Exception(result)
 
     test = [json.loads(i) for i in """
     [[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
@@ -237,7 +288,6 @@ def run_tests():
     """.strip().split('\n')]
     if (result := _get_sum(test)) != [[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]:
         raise Exception(result)
-
 
     test_inputs = """
     [[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
@@ -256,9 +306,9 @@ def run_tests():
     if result_1 != 4140:
         raise Exception(f"Test 1 did not pass, got {result_1}")
 
-    # result_2 = run_2(test_inputs)
-    # if result_2 != 0:
-    #     raise Exception(f"Test 2 did not pass, got {result_2}")
+    result_2 = run_2(test_inputs)
+    if result_2 != 3993:
+        raise Exception(f"Test 2 did not pass, got {result_2}")
 
 
 if __name__ == "__main__":
@@ -266,8 +316,8 @@ if __name__ == "__main__":
 
     input = read_inputs(18)
 
-    # result_1 = run_1(input)
-    # print(f"Finished 1 with result {result_1}")
+    result_1 = run_1(input)
+    print(f"Finished 1 with result {result_1}")
 
-    # result_2 = run_2(input)
-    # print(f"Finished 2 with result {result_2}")
+    result_2 = run_2(input)
+    print(f"Finished 2 with result {result_2}")
